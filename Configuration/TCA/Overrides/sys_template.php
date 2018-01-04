3c4f745fa4b60cad41a2ef51ca73b5037745b67b
Salvatore Eckel <salvaracer@gmx.de>
2018-01-04 22:09:11 +0100

    [INIT] Initial upload to git. Version 1.0.0

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,8 @@
+<?php
+defined('TYPO3_MODE') || die();
+
+/***************
+ * Default TypoScript
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3themes_bootstrap4', 'Configuration/TypoScript', 'TYPO3 Themes - Bootstrap4');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3themes_bootstrap4', 'Configuration/TypoScript/SetupNavigations', 'Bootstrap4 - Navs & Sidebars (1/2 - t3cms)');
---
+++ Configuration/TypoScript/setup.ts
@@ -0,0 +1,91 @@
+<INCLUDE_TYPOSCRIPT: source="DIR:EXT:t3themes_bootstrap4/Configuration/TypoScript/Setup/" extensions="ts,txt,typoscript">
+
+#### PAGE
+page = PAGE
+page {
+    typeNum = 0
+    #shortcutIcon = {$website.shortcutIcon}
+    10 = FLUIDTEMPLATE
+    10 {
+        templateName = TEXT
+        templateName.stdWrap.cObject = CASE
+        templateName.stdWrap.cObject {
+            key.data = pagelayout
+
+            pagets__bootstrap4clean = TEXT
+            pagets__bootstrap4clean.value = Default
+
+            default = TEXT
+            default.value = Default
+        }
+
+        templateRootPaths.0 = EXT:t3themes_bootstrap4/Resources/Private/Templates/Page/
+        partialRootPaths.0 = EXT:t3themes_bootstrap4/Resources/Private/Partials/Page/
+        layoutRootPaths.0 = EXT:t3themes_bootstrap4/Resources/Private/Layouts/Page/
+
+        ### DATA PREPROCESSING
+        dataProcessing {
+            10 = TYPO3\CMS\Frontend\DataProcessing\MenuProcessor
+            10 {
+                levels = 2
+                includeSpacer = 1
+                as = mainnavigation
+            }
+            20 = TYPO3\CMS\Frontend\DataProcessing\MenuProcessor
+            20 {
+                entryLevel = 1
+                levels = 2
+                expandAll = 0
+                includeSpacer = 1
+                as = subnavigation
+            }
+            30 = SalvatoreEckel\T3cms\DataProcessing\T3themesConfProcessor
+            30 {
+                fieldName = t3themes_conf
+                as = t3themesConf
+            }
+        }
+
+        ### VARIABLES
+        variables {
+            pageTitle = TEXT
+            pageTitle.data = page:title
+            rootPage = TEXT
+            rootPage.data = leveluid:0
+        }
+    }
+    meta {
+        viewport = width=device-width, initial-scale=1, shrink-to-fit=no
+        keywords.data = DB:pages:1:keywords 
+        keywords.override.field = keywords 
+        description.data = DB:pages:1:description 
+        description.override.field = description 
+        abstract.data = DB:pages:1:abstract 
+        abstract.override.field = abstract 
+        author.data = DB:pages:1:author 
+        author.override.field = author
+        web_author.data = DB:pages:1:author 
+        web_author.override.field = author
+    }
+
+    #includeCSSLibs {}
+    includeCSS {
+        bootstrap = EXT:t3themes_bootstrap4/Resources/Public/vendor/bootstrap/css/bootstrap.min.css
+        fontawesome = EXT:t3themes_bootstrap4/Resources/Public/vendor/font-awesome/css/font-awesome.min.css
+        simplelineicons = EXT:t3themes_bootstrap4/Resources/Public/vendor/simple-line-icons/css/simple-line-icons.css
+        theme_landingpage = EXT:t3themes_bootstrap4/Resources/Public/css/landing-page.min.css
+        theme_styles = EXT:t3themes_bootstrap4/Resources/Public/css/styles.css
+    }
+    #includeJSLibs {}
+    includeJSFooterlibs {
+        jquery = EXT:t3themes_bootstrap4/Resources/Public/vendor/jquery/jquery.min.js
+        jquery.forceOnTop = 1
+        bootstrap_bundle = EXT:t3themes_bootstrap4/Resources/Public/vendor/bootstrap/js/bootstrap.bundle.min.js
+        init_js = EXT:t3themes_bootstrap4/Resources/Public/js/init.js
+    }
+    #jsFooterInline {}
+    headerData {
+		12 = TEXT
+		12.value = <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    }
+}
